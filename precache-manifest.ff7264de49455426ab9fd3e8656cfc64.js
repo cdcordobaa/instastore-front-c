ac4dae33e8d653b520b2f6575c0aac1939a760b4
cdcordobaa <cdcordobaa@unal.edu.co>
2020-06-04 06:21:25 -0500

    Updates

---
+++ precache-manifest.ff7264de49455426ab9fd3e8656cfc64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0662041a1ee03d512a36985fd4d981e4",
+    "url": "/instastore-front-c/index.html"
+  },
+  {
+    "revision": "5d1e21e6092294047b5e",
+    "url": "/instastore-front-c/static/css/main.91cec054.chunk.css"
+  },
+  {
+    "revision": "3dddeab603425767740a",
+    "url": "/instastore-front-c/static/js/2.21849ac9.chunk.js"
+  },
+  {
+    "revision": "d2bcf9a9586332c90d24a7fe4e9ddcd2",
+    "url": "/instastore-front-c/static/js/2.21849ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1e21e6092294047b5e",
+    "url": "/instastore-front-c/static/js/main.2db00074.chunk.js"
+  },
+  {
+    "revision": "0d4ecbfbd3fbc3990660",
+    "url": "/instastore-front-c/static/js/runtime-main.e9910415.js"
+  }
+]);
